dd9c587a1f676b5d921cc461ed57b7b29dd6f1e7
Nagoor-K <p.nagoor@axelor.com>
2022-02-21 18:04:52 +0530

    RestEasy demo

---
+++ RestDemoApplication/src/main/java/com/nagoor/app/MessageApplication.java
@@ -0,0 +1,19 @@
+package com.nagoor.app;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+import com.nagoor.restful.MessageRestService;
+
+public class MessageApplication extends Application {
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public MessageApplication() {
+		singletons.add(new MessageRestService());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
