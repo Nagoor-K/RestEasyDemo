7b753ff0194c2358d4d9a163cb07e7800ae7fa6d
Nagoor-K <p.nagoor@axelor.com>
2022-02-24 12:12:09 +0530

    First tep into retful web services

--- RestDemoApplication/src/main/java/com/nagoor/restful/MessageRestService.java
+++ RestDemoApplication/src/main/java/com/nagoor/restful/MessageRestService.java
@@ -3,19 +3,17 @@ package com.nagoor.restful;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/message")
 public class MessageRestService {
 
 	@GET
-	@Path("/{param}")
-	public Response printMessage(@PathParam("param") String msg) {
-
-		String result = "Restful example : " + msg;
-
-		return Response.status(200).entity(result).build();
-
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMsg() {
+		return "Hello Nagoor Khan";
 	}
 
 }
